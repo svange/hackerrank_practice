066b56533c74ba6a748c586324326481ad1038ff
Samuel Vange <samuelvange@gmail.com>
2018-09-07 19:50:59 -0700

    new journey to the moon completed

--- JourneyToTheMoon.py
+++ JourneyToTheMoon.py
@@ -1,16 +1,28 @@
+import itertools
 import unittest, collections
 from typing import List
+from collections import defaultdict
 
 
-def journey_to_the_moon(astronaut_pairs: List[List[int]]):
+def journey_to_the_moon(n: int, astronaut: List[List[int]]):
+    # Construct the graph
     # Create an adj list
-    for x, y in astronaut_pairs:
-
-
-
-
-
     adj_list = []
+    for i in range(n):
+        # Each astronaut needs an empty list of neighbors
+        # Use a set to eliminate duplicate entries
+        adj_list.append(set())
+
+    for pair in astronaut:
+        # Each astronaut in each incoming astronaut pair is a neighbor of the other in that pair
+        a1, a2 = tuple(pair)
+        adj_list[a1].add(a2)
+        adj_list[a2].add(a1)
+
+    # Make the sets lists for ease of programming
+    adj_list = list(map(lambda x: list(x), adj_list))
+
+    # Get a table of connected components using BFS on each unvisited node
     connected_components = {}
     component = 0
     for node, neighbors in enumerate(adj_list):
@@ -26,15 +38,25 @@ def journey_to_the_moon(astronaut_pairs: List[List[int]]):
             q.extend(adj_list[current])
         component += 1
 
-    print(str(connected_components))
+    # We want a table that of the form |    cc: count
+    connected_components = connected_components.items()
+    cc_table = defaultdict(lambda: 0)
+    for astro, cc in connected_components:
+        cc_table[cc] += 1
+    combos = itertools.combinations(cc_table.keys(), 2)
 
 
-
+    # Calculate result
+    solution = 0
+    for c in combos:
+        solution += cc_table[c[0]] * cc_table[c[1]]
+    return solution
 
 
 class TestJourneyToTheMoon(unittest.TestCase):
     def test_solution_1(self):
-        self.assertEqual(journey_to_the_moon([[0, 1], [3, 2], [3, 4]]), 6)
+        # journey_to_the_moon(5, [[0, 1], [3, 2], [3, 4]])
+        self.assertEqual(journey_to_the_moon(5, [[0, 1], [3, 2], [3, 4]]), 6)
 
 
 if __name__ == '__main__':
